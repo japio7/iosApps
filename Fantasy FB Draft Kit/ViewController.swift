b6f8452bc72373623d9ee536d27980daa6f4ca07
Jared Pino <jaredpino@gmail.com>
2020-06-18 12:04:55 -0600

    Initial Commit

---
+++ Fantasy FB Draft Kit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fantasy FB Draft Kit
+//
+//  Created by Jared Pino on 6/18/20.
+//  Copyright © 2020 Jared Pino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
