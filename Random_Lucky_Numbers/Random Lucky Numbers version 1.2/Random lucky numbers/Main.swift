d83e48b06cd59aaacc7384596792904975d1e834
Jared Pino <39530089+japio7@users.noreply.github.com>
2021-08-24 21:39:34 -0600

    Add files via upload

---
+++ Random_Lucky_Numbers/Random Lucky Numbers version 1.2/Random lucky numbers/Main.swift
@@ -0,0 +1,34 @@
+//
+//  Main.swift
+//  Random lucky numbers
+//
+//  Created by Jared Pino on 11/11/18.
+//  Copyright © 2018 Jared Pino. All rights reserved.
+//
+
+import UIKit
+
+class Main: UIViewController {
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
