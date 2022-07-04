72eda6dfb98cc6ca6a8ed2200cfb9694d29bd862
Jared Pino <39530089+japio7@users.noreply.github.com>
2022-07-04 08:45:27 -0600

    Add files via upload

---
+++ Vehicle LoanCalc/VLC version 4.0/Vehicle LoanCalculator/File.swift
@@ -0,0 +1,128 @@
+//
+//  File.swift
+//  Vehicle LoanCalculator
+//
+//  Created by Jared Pino on 3/14/20.
+//  Copyright © 2020 Jared Pino. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+private var __maxLengths = [UITextField: Int]()
+extension UITextField {
+    @IBInspectable var maxLength: Int {
+        get {
+            guard let l = __maxLengths[self] else {
+                return 150 // (global default-limit. or just, Int.max)
+            }
+            return l
+        }
+        set {
+            __maxLengths[self] = newValue
+            addTarget(self, action: #selector(fix), for: .editingChanged)
+        }
+    }
+    @objc func fix(textField: UITextField) {
+        let t = textField.text
+        textField.text = t?.safelyLimitedTo(length: maxLength)
+    }
+}
+
+// Text Field Features
+@IBDesignable class BorderedTextField: UITextField {
+
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            if let bColor = borderColor {
+                self.layer.borderColor = bColor.cgColor
+            }
+        }
+    }
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+}
+
+// Button Features
+@IBDesignable class BorderedButton: UIButton {
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            if let bColor = borderColor {
+                self.layer.borderColor = bColor.cgColor
+            }
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+           set {
+               layer.cornerRadius = newValue
+           }
+           get {
+               return layer.cornerRadius
+           }
+       }
+    
+    override var isHighlighted: Bool {
+        didSet {
+            guard let currentBorderColor = borderColor else {
+                return
+            }
+            
+            let fadedColor = currentBorderColor.withAlphaComponent(0.2).cgColor
+            
+            if isHighlighted {
+                layer.borderColor = fadedColor
+            } else {
+                
+                self.layer.borderColor = currentBorderColor.cgColor
+                
+                let animation = CABasicAnimation(keyPath: "borderColor")
+                animation.fromValue = fadedColor
+                animation.toValue = currentBorderColor.cgColor
+                animation.duration = 0.4
+                self.layer.add(animation, forKey: "")
+            }
+        }
+    }
+}
+
+extension String
+{
+    func safelyLimitedTo(length n: Int)->String {
+        if (self.count <= n) {
+            return self
+        }
+        return String( Array(self).prefix(upTo: n) )
+    }
+}
+
+// To not allow copy and paste.
+class TextField: UITextField {
+
+    override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+    
+    if action == #selector(copy(_:)) || action == #selector(selectAll(_:)) || action == #selector(paste(_:)) {
+    return false
+    }
+    return super.canPerformAction(action, withSender: sender)
+    }
+}
+
